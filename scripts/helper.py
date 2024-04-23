bb3f671f26be4d57118438773864de5cfd8caabb
sewerynszydelko <seweryn494a@gmail.com>
2024-04-23 20:54:34 +0200

    Create and add 2 files for help build new func for project

---
+++ scripts/helper.py
@@ -0,0 +1,19 @@
+from pynput import mouse
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    def on_click(x, y, button, pressed):
+        if button == button.middle:
+            listner.stop()
+        print(f"x = {x} y = {y}")
+
+
+    with mouse.Listener(on_click=on_click) as listner:
+        listner.join()
+
