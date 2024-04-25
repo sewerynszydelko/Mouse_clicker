a9dc54f44dfb4b716db38f9e473330569ee65a0f
sewerynszydelko <seweryn494a@gmail.com>
2024-04-25 22:17:25 +0200

    Added concept of mouse and darwing

--- scripts/m_k_helper.py
+++ scripts/m_k_helper.py
@@ -3,6 +3,7 @@ from pynput import keyboard, mouse
 
 if __name__ == "__main__":
 
+    # Keyboard
     controler = keyboard.Controller()
 
     controler.press(keyboard.Key.cmd_l)
@@ -19,9 +20,42 @@ if __name__ == "__main__":
     with controler.pressed(keyboard.Key.cmd_l):
         controler.release(keyboard.Key.up)
 
-    # controler.release(keyboard.Key.up)
-    # controler.press(keyboard.Key.cmd_l)
-    # controler.press(keyboard.Key.page_up)
-    # time.sleep(0.5)
-    # controler.release(keyboard.Key.page_up)
-    # controler.release(keyboard.Key.cmd_l)
+    time.sleep(1)
+
+    # Mouse
+    mouse_controler = mouse.Controller()
+
+    mouse_controler.position = (373, 105)
+    time.sleep(0.5)
+    mouse_controler.click(mouse.Button.left, 1)
+    time.sleep(0.5)
+    mouse_controler.position = (366, 143)
+    time.sleep(0.5)
+    mouse_controler.click(mouse.Button.left, 1)
+    time.sleep(0.5)
+    mouse_controler.position = (890, 64)
+    time.sleep(0.5)
+    mouse_controler.click(mouse.Button.left, 1)
+    time.sleep(0.5)
+    mouse_controler.position = (705, 91)
+    time.sleep(0.5)
+    mouse_controler.click(mouse.Button.left, 1)
+    time.sleep(0.5)
+    mouse_controler.position = (734, 222)
+    time.sleep(0.5)
+    mouse_controler.click(mouse.Button.left, 1)
+    time.sleep(1)
+
+    # Drawing
+    mouse_controler.position = (800, 300)
+    mouse_controler.press(mouse.Button.left)
+    time.sleep(1)
+    mouse_controler.move(50, 100)
+    mouse_controler.release(mouse.Button.left)
+    mouse_controler.press(mouse.Button.left)
+    time.sleep(1)
+    mouse_controler.move(0, 400)
+    time.sleep(1)
+    mouse_controler.move(200, 0)
+    time.sleep(1)
+    mouse_controler.release(mouse.Button.left)
