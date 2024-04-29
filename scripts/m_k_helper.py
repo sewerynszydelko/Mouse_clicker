15297bfefba31b8b8732951847690226a58084f0
sewerynszydelko <seweryn494a@gmail.com>
2024-04-29 22:11:11 +0200

    Added docsting to helper

--- scripts/m_k_helper.py
+++ scripts/m_k_helper.py
@@ -11,6 +11,10 @@ class Controller():
         self.keyboard = keyboard.Controller()
 
     def click_key(self, key_to_pres_relise):
+        """ Pres and relise key of given key
+        Args:
+            key_to_pres_relise (Keyboard.Key.'name_key'): key from pynput keyborad.Key
+        """
         sleep(self.wait)
         self.keyboard.press(key_to_pres_relise)
         sleep(self.wait)
@@ -18,27 +22,56 @@ class Controller():
         sleep(self.wait)
 
     def click_at_position(self, cordinate_x, cordinate_y, preses=1, button=mouse.Button.left):
+        """ Click at given position
+        Args:
+            cordinate_x (int): cordiante x of pixels
+            cordinate_y (int): cordinate y of pixels
+            preses (int, optional): number of presses. Defaults to 1.
+            button (_type_, optional): button to use. Defaults to mouse.Button.left.
+        """
         self.set_positon(cordinate_x, cordinate_y)
         sleep(self.wait)
         self.mouse.click(button, preses)
         sleep(self.wait)
 
     def set_positon(self, cordinate_x, cordinate_y):
+        """Set posiotn of mouse
+        Args:
+            cordinate_x (int)
+            cordinate_y (int)
+        """
         self.mouse.position = (cordinate_x, cordinate_y)
 
-    def pres_move_cursor(self,position_x, position_y, move_x, move_y,button=mouse.Button.left):
+    def pres_move_cursor(self, position_x, position_y, move_x, move_y, button=mouse.Button.left):
+        """ Hold button and move it
+        Args:
+            position_x (int)
+            position_y (int)
+            move_x (int): amout of pixels to move
+            move_y (int): amout of pixels to move
+            button (int, optional): Defaults to mouse.Button.left.
+        """
         self.set_positon(position_x, position_y)
         self.mouse.press(button)
         sleep(self.wait)
-        self.mouse.move(move_x,move_y)
+        self.mouse.move(move_x, move_y)
         sleep(self.wait)
         self.mouse.release(button)
 
     def write_sentenc(self, sentence):
+        """ Writing full sentence
+        Args:
+            sentence (str): Sentenc to write
+        """
         sleep(self.wait)
         self.keyboard.type(sentence)
 
     def hold_and_press_key(self, key_to_hold, key_to_press):
+        """ Holding and presing 2 difrent key
+        Args:
+            key_to_hold (_type_): Key to hold
+            key_to_press (_type_): Key to press
+        """
         self.keyboard.press(key_to_hold)
         sleep(self.wait)
         self.keyboard.press(key_to_press)
@@ -67,7 +100,7 @@ if __name__ == "__main__":
 
     # # Drawing
 
-    controller.pres_move_cursor(800,300,500,100)
+    controller.pres_move_cursor(800, 300, 500, 100)
     # mouse_controler.position = (800, 300)
     # mouse_controler.press(mouse.Button.left)
     # time.sleep(1)
