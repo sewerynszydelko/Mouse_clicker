ba1a9f62f0dc2c824b3dee74f2cffd83641f2645
sewerynszydelko <seweryn494a@gmail.com>
2024-05-01 20:07:07 +0200

    Added doctsing in place in func

--- scripts/helper.py
+++ scripts/helper.py
@@ -2,10 +2,12 @@ from pynput import mouse
 
 
 if __name__ == "__main__":
-    def on_click(x, y, button, pressed):
-        if button == button.middle:
-            listner.stop()
-        print(f"x = {x} y = {y}")
+    def mouse_helper():
+        """ Print info about place of mouse by pres, if want exit pres midles mouse button"""
+        def on_click(x, y, button, pressed):
+            if button == button.middle:
+                listner.stop()
+            print(f"x = {x} y = {y}")
 
-    with mouse.Listener(on_click=on_click) as listner:
-        listner.join()
+        with mouse.Listener(on_click=on_click) as listner:
+            listner.join()
